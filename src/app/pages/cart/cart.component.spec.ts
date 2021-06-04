3f0c2e4006c2c27e65a7dadcebaeb9ab122db728
Sebas <seplarui@gmail.com>
2021-06-04 21:24:59 +0200

    First commit

---
+++ src/app/pages/cart/cart.component.spec.ts
@@ -0,0 +1,7 @@
+import { ComponentFixture } from '@angular/core/testing';
+import { CartComponent } from './cart.component';
+describe('Cart component', () => {
+  let component: CartComponent;
+  let fixture: ComponentFixture<CartComponent>;
+
+});
