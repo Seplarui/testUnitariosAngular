d0270f2f027755673d7f8b4fadd5d932d3efb2e8
Sebas <seplarui@gmail.com>
2021-06-04 21:34:02 +0200

    beforeEach()

--- src/app/pages/cart/cart.component.spec.ts
+++ src/app/pages/cart/cart.component.spec.ts
@@ -1,7 +1,32 @@
-import { ComponentFixture } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CartComponent } from './cart.component';
+import { BookService } from '../../services/book.service';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+
 describe('Cart component', () => {
   let component: CartComponent;
   let fixture: ComponentFixture<CartComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      declarations: [
+        CartComponent
+      ],
+      providers: [
+        BookService
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
+    });
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
 });
