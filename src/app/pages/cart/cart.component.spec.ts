9653e1655dcd2bb4875c0ea0a2526216e902d383
Sebas <seplarui@gmail.com>
2021-06-05 19:35:16 +0200

    service

--- src/app/pages/cart/cart.component.spec.ts
+++ src/app/pages/cart/cart.component.spec.ts
@@ -3,6 +3,31 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CartComponent } from './cart.component';
 import { BookService } from '../../services/book.service';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { Book } from '../../models/book.model';
+
+const listBook: Book[] = [
+  {
+    name: '',
+    author: '',
+    isbn: '',
+    price: 15,
+    amount: 2
+  },
+  {
+    name: '',
+    author: '',
+    isbn: '',
+    price: 20,
+    amount: 1
+  },
+  {
+    name: '',
+    author: '',
+    isbn: '',
+    price: 8,
+    amount: 7
+  }
+];
 
 describe('Cart component', () => {
   let component: CartComponent;
@@ -31,6 +56,19 @@ describe('Cart component', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('getTotalPrice returns an amount', () => {
+    const totalPrice = component.getTotalPrice(listBook);
+    expect(totalPrice).toBeGreaterThan(0);
+    expect(totalPrice).not.toBeNull();
+  });
+
+  it('onInputNumberChange increments correctly', () => {
+    const action = 'plus';
+    const book = listBook[0];
+    const service = fixture.debugElement.injector.get(BookService);
+
   });
 
 });
